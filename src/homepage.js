46b9560533f8aacf678e01ab8ce070331533439a
Triston Land <tristonland@gmail.com>
2025-01-10 01:49:36 -0700

    add styles to entry point

---
+++ src/homepage.js
@@ -0,0 +1,4 @@
+export default () => {const content = document.querySelector(".content");
+const pageContent = document.createElement("div");
+pageContent.classList.add("pageContent")
+content.appendChild(pageContent)}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./style.css" // be sure to import styles for webpack
+import homepage from "./homepage";
+homepage()
