5558b955177b122700ac6ef1855e541b639a24fa
Triston Land <tristonland@gmail.com>
2025-01-10 01:13:15 -0700

    add header and have webpack track template html changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { watchFile } = require("fs");
 
 module.exports = {
   mode: "development",
@@ -9,6 +10,10 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer:{
+    watchFiles: ["./src/template.html"]
   },
   plugins: [
     new HtmlWebpackPlugin({
