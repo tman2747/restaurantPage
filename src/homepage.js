4d61262fbfcf0672cade9ace25d935135e364ff3
Triston Land <tristonland@gmail.com>
2025-01-13 17:02:59 -0700

    make page content expand to the full page

--- src/homepage.js
+++ src/homepage.js
@@ -1,4 +1,4 @@
-export default () => {const content = document.querySelector(".content");
+export default () => {const content = document.querySelector(".middle-content");
 const pageContent = document.createElement("div");
 pageContent.classList.add("pageContent")
 content.appendChild(pageContent)}
